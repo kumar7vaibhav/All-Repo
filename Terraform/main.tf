8890b8b352cfd442f0d76073b58357af97fbfcfb
Kumar Vaibhav <75883783+kumar7vaibhav@users.noreply.github.com>
2023-12-28 18:47:14 +0530

    new file:   Terraform/auto.tfvars 	modified:   Terraform/main.tf 	new file:   Terraform/variables.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,58 +1,57 @@
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.34.0"
-    }
-  }
+ required_providers {
+   google = {
+     source = "hashicorp/google"
+     version = var.google_version
+   }
+ }
 }
 
-
 module "google_service_account" {
-    source = "./modules/service_account"
-    account_id  = "test-gcf-sa"
-    display_name = "Test Service Account"
-    project    = "terraform-pubsub-409511"
+   source = "./modules/service_account"
+   account_id = var.account_id
+   display_name = var.display_name
+   project   = var.project
 }
 
 module "google_pubsub_topic" {
-    source = "./modules/pubsub"
-    topic_name = "functions2-topic"
-    project  = "terraform-pubsub-409511"
+   source = "./modules/pubsub"
+   topic_name = var.topic_name
+   project = var.project
 }
 
 module "google_storage_bucket" {
-    source = "./modules/bucket"
-    bucket_name = "2165421516-gcf-source"
-    location = "asia-south2"
-    project = "terraform-pubsub-409511" 
-    uniform_bucket_level_access = true
-    output_path =  "/tmp/function-source.zip"
-    type = "zip"
-    source_dir = "./helloPubSub"
-    bucket_object_name = "function-source.zip"
+   source = "./modules/bucket"
+   bucket_name = var.bucket_name
+   location = var.location
+   project = var.project
+   uniform_bucket_level_access = var.uniform_bucket_level_access
+   output_path = var.output_path
+   type = var.type
+   source_dir = var.source_dir
+   bucket_object_name = var.bucket_object_name
 }
 
 module "google_cloudfunctions2_function" {
-  source = "./modules/function"
+ source = "./modules/function"
 
-  function_name = "function"
-  function_location = "asia-south2"
-  project = "terraform-pubsub-409511"
-  runtime = "nodejs16"
-  entry_point = "helloPubSub"
-  bucket_name = module.google_storage_bucket.bucket_name
-  bucket_object_name = module.google_storage_bucket.bucket_object_name
+ function_name = var.function_name
+ function_location = var.function_location
+ project = var.project
+ runtime = var.runtime
+ entry_point = var.entry_point
+ bucket_name = module.google_storage_bucket.bucket_name
+ bucket_object_name = module.google_storage_bucket.bucket_object_name
 
-  max_instance_count = 3
-  min_instance_count = 1
-  available_memory = "256M"
-  timeout_seconds = 60
-  ingress_settings = "ALLOW_INTERNAL_ONLY"
-  all_traffic_on_latest_revision = true
-  service_account_email = module.google_service_account.service_account_email
-  trigger_region = "asia-south2"
-  event_type = "google.cloud.pubsub.topic.v1.messagePublished"
-  pubsub_topic_id = module.google_pubsub_topic.pubsub_topic_id
-  retry_policy = "RETRY_POLICY_RETRY"
+ max_instance_count = var.max_instance_count
+ min_instance_count = var.min_instance_count
+ available_memory = var.available_memory
+ timeout_seconds = var.timeout_seconds
+ ingress_settings = var.ingress_settings
+ all_traffic_on_latest_revision = var.all_traffic_on_latest_revision
+ service_account_email = module.google_service_account.service_account_email
+ trigger_region = var.trigger_region
+ event_type = var.event_type
+ pubsub_topic_id = module.google_pubsub_topic.pubsub_topic_id
+ retry_policy = var.retry_policy
 }
---
+++ Terraform/variables.tf
@@ -0,0 +1,124 @@
+variable "google_version" {
+ description = "The version of Google provider"
+ default    = ">= 4.34.0"
+}
+
+variable "account_id" {
+ description = "The ID of the Google service account"
+ default    = "test-gcf-sa"
+}
+
+variable "display_name" {
+ description = "The display name of the Google service account"
+ default    = "Test Service Account"
+}
+
+variable "project" {
+ description = "The Google Cloud project ID"
+ default    = "terraform-pubsub-409511"
+}
+
+variable "topic_name" {
+ description = "The name of the PubSub topic"
+ default    = "functions2-topic"
+}
+
+variable "bucket_name" {
+ description = "The name of the storage bucket"
+ default    = "2165421516-gcf-source"
+}
+
+variable "location" {
+ description = "The location of the resources"
+ default    = "asia-south2"
+}
+
+variable "uniform_bucket_level_access" {
+ description = "Whether to enable uniform bucket level access"
+ default    = true
+}
+
+variable "output_path" {
+ description = "The path to store the function source code"
+ default    = "/tmp/function-source.zip"
+}
+
+variable "type" {
+ description = "The type of the function source code"
+ default    = "zip"
+}
+
+variable "source_dir" {
+ description = "The directory containing the function source code"
+ default    = "./helloPubSub"
+}
+
+variable "bucket_object_name" {
+ description = "The name of the object in the storage bucket"
+ default    = "function-source.zip"
+}
+
+variable "function_name" {
+ description = "The name of the Cloud Function"
+ default    = "function"
+}
+
+variable "function_location" {
+ description = "The location of the Cloud Function"
+ default    = "asia-south2"
+}
+
+variable "runtime" {
+ description = "The runtime of the Cloud Function"
+ default    = "nodejs16"
+}
+
+variable "entry_point" {
+ description = "The entry point of the Cloud Function"
+ default    = "helloPubSub"
+}
+
+variable "max_instance_count" {
+ description = "The maximum number of instances for the Cloud Function"
+ default    = 3
+}
+
+variable "min_instance_count" {
+ description = "The minimum number of instances for the Cloud Function"
+ default    = 1
+}
+
+variable "available_memory" {
+ description = "The amount of memory available for the Cloud Function"
+ default    = "256M"
+}
+
+variable "timeout_seconds" {
+ description = "The timeout for the Cloud Function"
+ default    = 60
+}
+
+variable "ingress_settings" {
+ description = "The ingress settings for the Cloud Function"
+ default    = "ALLOW_INTERNAL_ONLY"
+}
+
+variable "all_traffic_on_latest_revision" {
+ description = "Whether to route all traffic to the latest revision"
+ default    = true
+}
+
+variable "trigger_region" {
+ description = "The region of the trigger"
+ default    = "asia-south2"
+}
+
+variable "event_type" {
+ description = "The type of the event trigger"
+ default    = "google.cloud.pubsub.topic.v1.messagePublished"
+}
+
+variable "retry_policy" {
+ description = "The retry policy for the event trigger"
+ default    = "RETRY_POLICY_RETRY"
+}
