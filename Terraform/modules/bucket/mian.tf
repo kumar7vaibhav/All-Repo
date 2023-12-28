4b716f7b5ef3d948fbe96b375134f278a01f8bb3
Kumar Vaibhav <75883783+kumar7vaibhav@users.noreply.github.com>
2023-12-28 13:43:31 +0530

    Delete Terraform directory

--- Terraform/modules/bucket/mian.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "default" {
-  name                        = var.bucket_name
-  location                    = var.location
-  uniform_bucket_level_access = var.uniform_bucket_level_access
-}
