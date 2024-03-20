cb3efa9b020c9a5d2b7576b4d364321994d8acf5
Kumar Vaibhav <75883783+kumar7vaibhav@users.noreply.github.com>
2024-03-20 19:44:12 +0530

    Update Sum-ContentDBSize.ps1

--- Sum-ContentDBSize.ps1
+++ Sum-ContentDBSize.ps1
@@ -1,28 +1,9 @@
-# Import SQL Server module
-Import-Module SQLPS -DisableNameChecking
-
-# Set SQL Server instance name
-$SQLInstance = "YourSQLServerInstance"
-
-# Connect to SQL Server
-$SQLConnection = New-Object System.Data.SqlClient.SqlConnection
-$SQLConnection.ConnectionString = "Server=$SQLInstance;Database=master;Integrated Security=True"
-$SQLConnection.Open()
-
-# Get SharePoint content databases
-$SQLCommand = $SQLConnection.CreateCommand()
-$SQLCommand.CommandText = "SELECT name FROM sys.databases WHERE name LIKE 'YourSPContentDBPrefix%'"
-$ContentDatabases = $SQLCommand.ExecuteReader()
-
-# Loop through each content database and calculate size
-while ($ContentDatabases.Read()) {
-    $ContentDBName = $ContentDatabases["name"]
-    
-    $SQLCommand.CommandText = "SELECT SUM(size) * 8 / 1024 AS SizeMB FROM sys.master_files WHERE database_id = DB_ID('$ContentDBName')"
-    $DBSize = $SQLCommand.ExecuteScalar()
-    
-    Write-Host "Database: $ContentDBName - Size: $DBSize MB"
-}
-
-# Close SQL connection
-$SQLConnection.Close()
+SELECT
+    DB_NAME(database_id) AS DatabaseName,
+    SUM(size) * 8 / 1024 AS Size_MB
+FROM
+    sys.master_files
+WHERE
+    DB_NAME(database_id) LIKE 'WSS_Content%'
+GROUP BY
+    database_id;
