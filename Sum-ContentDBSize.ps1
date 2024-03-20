b61f4c8613327d283e7c97f6ad4049d6315e012f
Kumar Vaibhav <75883783+kumar7vaibhav@users.noreply.github.com>
2024-03-20 19:46:59 +0530

    Update Sum-ContentDBSize.ps1

--- Sum-ContentDBSize.ps1
+++ Sum-ContentDBSize.ps1
@@ -7,3 +7,17 @@ WHERE
     DB_NAME(database_id) LIKE 'WSS_Content%'
 GROUP BY
     database_id;
+
+
+SELECT
+    SUM(Size_MB) AS TotalSize_MB
+FROM
+    (SELECT
+        DB_NAME(database_id) AS DatabaseName,
+        SUM(size) * 8 / 1024 AS Size_MB
+    FROM
+        sys.master_files
+    WHERE
+        DB_NAME(database_id) LIKE 'WSS_Content%'
+    GROUP BY
+        database_id) AS ContentSizes;
